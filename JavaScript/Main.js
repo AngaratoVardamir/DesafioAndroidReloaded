10e991b7746a9f6b444edd52676e3625ae08decf
Gustavo Augusto <Gus191798@gmail.com>
2025-01-02 00:58:03 -0300

    Enhancement - Melhorias na Interatividade e Estilo da Página História do Android
    + Adicionados links de navegação para facilitar o acesso às seções do site.
    
    + Implementado botão para abrir e fechar a página História do Android, melhorando a navegação.
    
    + Ajustados efeitos de animação e hover para uma experiência mais dinâmica.
    
    + Corrigidas marcações de links, garantindo melhor semântica e acessibilidade.
    
    + Novas imagens inseridas para enriquecer o conteúdo visual.
    
    + Implementada interação aprimorada no site, tornando-o mais fluido e responsivo.

--- JavaScript/Main.js
+++ JavaScript/Main.js
@@ -24,10 +24,34 @@ function ControleLogo() {
 
 ControleLogo()
 
-const ButtonsBlocked = [...document.getElementsByClassName('Blocked')];
+function ButtonBlock(){
+    const ButtonsBlocked = [...document.getElementsByClassName('Blocked')];
 
 
-ButtonsBlocked.map((BB) => {
-    BB.children[0].innerHTML = (`<i class="bi bi-lock"></i>`);
-    BB.children[1].innerHTML = (`Bloqueado`);
+    ButtonsBlocked.map((BB) => {
+        BB.children[0].innerHTML = (`<i class="bi bi-lock"></i>`);
+        BB.children[1].innerHTML = (`Bloqueado`);
+    })
+}
+ButtonBlock()
+
+const ButtonX = [...document.getElementsByClassName('ButtonX')];
+const Frame = [...document.getElementsByClassName('Frame')];
+
+ButtonX.map((BX) => {
+    BX.addEventListener("click",()=>{
+        Frame.map((FM)=>{
+            FM.style.display = "none";
+        })
+    })
 })
+
+const ButtonWindow = [...document.getElementsByClassName('BunActive')];
+ButtonWindow.map((WB, Wix) => {
+    console.log(WB)
+    WB.addEventListener("click",()=>{
+        Frame.map((FM)=>{
+            FM.style.display = "flex";
+        })
+    })
+});
